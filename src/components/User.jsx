b8374ed52acc2454836fe898be801b9956d99501
DESKTOP-HND3DGB\Tomek <tomasz.siniak@softwarehouse.com>
2018-06-11 10:09:18 +0200

    added function to refresh repositories

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -8,7 +8,7 @@ export const User = (props) => (
         <p>Login: {props.user.login}</p>
         <p>Created at: {props.user.created}</p>
         <button onClick={ () => showUserRepos(props.user.login)}>Show repos</button>
-        <button>Refresh</button> 
+        <button disabled={props.user.repos.length === 0} onClick={ () => showUserRepos(props.user.login)}>Refresh</button> 
       </div>
       <div className="repos">
         {props.user.repos.map( (repo, key) => {
